f5bcd5d6ebcda28e1876fa417ef0907ac3a7628a
furkanemrea <furkanemrealtintas@gmail.com>
2022-08-28 04:43:40 +0300

    docker file added and start-up configuration

---
+++ TestApi/Controllers/CustomerController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TestApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CustomerController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok("Geldi");
+        }
+    }
+}
