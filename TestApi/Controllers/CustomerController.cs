f3226e4acd37fd2eb6ce5614747441964bf31013
furkanemrea <furkanemrealtintas@gmail.com>
2022-08-28 05:37:21 +0300

    some changes

--- TestApi/Controllers/CustomerController.cs
+++ TestApi/Controllers/CustomerController.cs
@@ -7,10 +7,13 @@ namespace TestApi.Controllers
     public class CustomerController : ControllerBase
     {
         [HttpGet]
+        [Route("first-test")]
         public IActionResult Test()
         {
             return Ok("Test");
         }
+        [HttpGet]
+        [Route("second-test")]
         public IActionResult Test2()
         {
             return Ok("Jenkins Executed"); 
